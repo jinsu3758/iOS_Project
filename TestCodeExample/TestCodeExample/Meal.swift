520632c2096d6fb588045acd6dc9e983a73fa60b
jinsu3758 <jinsu3758@naver.com>
2020-06-11 09:27:20 +0900

    테스트코드 작성 추가

---
+++ TestCodeExample/TestCodeExample/Meal.swift
@@ -0,0 +1,26 @@
+//
+//  Meal.swift
+//  TestCodeExample
+//
+//  Created by 박진수 on 2020/06/04.
+//  Copyright © 2020 박진수. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meal {
+    var name = ""
+    var photo: UIImage?
+    var rating = 0
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        if name.isEmpty {
+            return nil
+   
+    }
+    
+    convenience init?() {
+        self.init(name: "Unknown", photo: nil, rating: 0)
+    }
+}
---
+++ TestCodeExample/TestCodeExampleTests/FoodTrackerTests.swift
@@ -0,0 +1,52 @@
+//
+//  FoodTrackerTests.swift
+//  TestCodeExampleTests
+//
+//  Created by 박진수 on 2020/06/03.
+//  Copyright © 2020 박진수. All rights reserved.
+//
+
+import XCTest
+@testable import TestCodeExample
+
+class FoodTrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_createMeal() {
+        let meal = Meal()
+        
+        XCTAssertNotNil(meal)
+    }
+    
+    func test_mealInitialization() {
+        let meal = Meal(name: "First meal", photo: nil, rating: 5)
+        
+        XCTAssertNotNil(meal)
+    }
+    
+    func test_mealInitWithoutName() {
+        let meal = Meal(name: "", photo: nil, rating: 5)
+        
+        XCTAssertNil(meal)
+    }
+    
+    func test_initWithNegativeRank() {
+        let meal = Meal(name: "meal", photo: nil, rating: -1)
+        
+        XCTAssertNil(meal)
+    }
+    
+    func test_initWithNegativeRank() {
+        let meal = Meal(name: "meal", photo: nil, rating: -1)
+        
+        XCTAssertNil(meal)
+    }
+
+}
