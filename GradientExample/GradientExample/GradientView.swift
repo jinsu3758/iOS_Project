6a8942c75c8e16e987eebbf4e806e8ed6cffdf6e
jinsu3758 <jinsu3758@naver.com>
2020-10-19 19:52:27 +0900

    gradientView 구현 완료 (Rx 제외)

---
+++ GradientExample/GradientExample/GradientView.swift
@@ -0,0 +1,87 @@
+//
+//  GradientView.swift
+//  GradientExample
+//
+//  Created by 박진수 on 2020/10/19.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    private var colors: [CGColor]
+    private var style: GradientStyle
+    private let gradientLayer = CAGradientLayer()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(frame: CGRect, style: GradientStyle, colors: [CGColor], locations: [NSNumber]? = nil) {
+        self.colors = colors
+        self.style = style
+        gradientLayer.locations = locations
+        super.init(frame: frame)
+        updateGradientLayer()
+    }
+    
+    private func updateGradientLayer() {
+        gradientLayer.frame = self.frame
+        gradientLayer.colors = colors
+        
+        switch style {
+        case .horizontal:
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        case .vertical:
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        case .conic:
+            gradientLayer.type = .conic
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
+        case .leftDiagonal:
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        case .rightDiagonal:
+            gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        case .radial:
+            gradientLayer.type = .radial
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        }
+        
+        self.layer.addSublayer(gradientLayer)
+    }
+    
+    func setAnimation(_ newColors: [CGColor], withDuration duration: TimeInterval = 1, repeatCount: Float = .infinity, timingFunctionName name: CAMediaTimingFunctionName? = nil ) {
+        let animation = CABasicAnimation(keyPath: "colors")
+        animation.fromValue = colors
+        animation.toValue = newColors
+        animation.duration = duration
+        animation.timingFunction = CAMediaTimingFunction(name: name ?? .linear)
+        animation.repeatCount = repeatCount
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.autoreverses = true
+        animation.delegate = self
+        
+        gradientLayer.add(animation, forKey: "colorsChangeAnimation")
+    }
+    
+    
+}
+
+// TODO: - Rx observable로 바꿔서 구독하게 하긴
+extension GradientView: CAAnimationDelegate {
+    func animationDidStart(_ anim: CAAnimation) {
+        print("start!")
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        print("stop!")
+    }
+    
+    
+}
+
--- GradientExample/GradientExample/ViewController.swift
+++ GradientExample/GradientExample/ViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
+enum GradientStyle {
+    /// 사선
+    case leftDiagonal
+    case rightDiagonal
+    case horizontal
+    case vertical
+    /// 원뿔형의
+    case conic
+    /// 방사형의
+    case radial
+    
+}
+
 class ViewController: UIViewController {
+    @IBOutlet weak var gradientView: UIView!
+    var gradient: GradientView!
+    var colors: [CGColor] = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.yellow.cgColor]
+    let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let gradientLayer = CAGradientLayer()
+        gradient = GradientView(frame: gradientView.bounds, style: .leftDiagonal, colors: colors)
+        gradientView.addSubview(gradient)
+        
+        
+//        let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.frame
         gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.yellow.cgColor]
         gradientLayer.locations = [0, 0.3, 1]
+//        gradientView.layer.addSublayer(gradientLayer)
         
         // 수직은 default
         // 수평
@@ -30,17 +52,68 @@ class ViewController: UIViewController {
 //        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
         
         
-        // 대각선
+        // 왼쪽 대각선
         
 //        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
 //        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         
-        self.view.layer.addSublayer(gradientLayer)
         
+//         오른쪽 대각선
+        
+        gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        
+//        self.view.layer.addSublayer(gradientLayer)
+        
+        // 방사형
+//        gradientLayer.type = .radial
+//        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.5)
+//        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        
+//        setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor])
         
         // Do any additional setup after loading the view.
     }
+    
+    func setAnimation(_ newColors: [CGColor], withDuration duration: TimeInterval = 1, repeatCount: Float = .infinity, timingFunctionName name: CAMediaTimingFunctionName? = nil ) {
+        let colorAnimation = CABasicAnimation(keyPath: "colors")
+        colorAnimation.fromValue = colors
+        colorAnimation.toValue = newColors
+        colorAnimation.duration = duration
+//        colorAnimation.isRemovedOnCompletion = false
+        colorAnimation.fillMode = CAMediaTimingFillMode.forwards
+        colorAnimation.timingFunction = CAMediaTimingFunction(name: name ?? .linear)
+        colorAnimation.repeatCount = repeatCount
+        colorAnimation.autoreverses = true
+        
+        gradientLayer.removeAllAnimations()
+        gradientLayer.add(colorAnimation, forKey: "colorsChangeAnimation")
+        
+    }
 
-
+    @IBAction func startAnimation(_ sender: Any) {
+        gradient.setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor], withDuration: 1.5)
+    }
+    
+    @IBAction func stopAnimation(_ sender: Any) {
+        gradientLayer.colors = [UIColor.black.cgColor, UIColor.orange.cgColor, UIColor.white.cgColor]
+        setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor])
+    }
+    
+    @IBAction func linear(_ sender: Any) {
+        gradient.setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor], withDuration: 2, timingFunctionName: .linear)
+    }
+    
+    @IBAction func easeIn(_ sender: Any) {
+        gradient.setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor], withDuration: 2, timingFunctionName: .easeIn)
+    }
+    
+    @IBAction func easeOut(_ sender: Any) {
+        gradient.setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor], withDuration: 2, timingFunctionName: .easeOut)
+    }
+    
+    @IBAction func easeInOut(_ sender: Any) {
+        gradient.setAnimation([UIColor.green.cgColor, UIColor.gray.cgColor, UIColor.brown.cgColor], withDuration: 2, timingFunctionName: .easeInEaseOut)
+    }
 }
 
