ac540e4df63e58abbb2def347d8c9e48d5efc1c9
jinsu3758 <jinsu3758@naver.com>
2021-01-15 15:15:14 +0900

    main queue 테스트

--- DispatchQueue.playground/Contents.swift
+++ DispatchQueue.playground/Contents.swift
@@ -3,37 +3,47 @@ import Foundation
 
 
 let dispatch = DispatchQueue.global()
+let mainqueue = DispatchQueue.main
 let new_dispatch = DispatchQueue.init(label: "new")
 
-dispatch.async {
+//dispatch.async {
+//    for i in 0..<5 {
+//        print("async 1 : \(i)")
+//    }
+//}
+mainqueue.async {
     for i in 0..<5 {
-        print("async 1 : \(i)")
+        print("main async 1 : \(i)")
     }
 }
 
-new_dispatch.sync {
-    for i in 0..<5 {
-        print("new sync 1 : \(i)")
-    }
+for i in 0..<5 {
+    print("main 1 : \(i)")
 }
 
-dispatch.sync {
-    for i in 0..<5 {
-        print("sync 1 : \(i)")
-    }
-}
-
-dispatch.async {
-    for i in 0..<5 {
-        print("async 2 : \(i)")
-    }
-}
-
-dispatch.async {
-    for i in 0..<5 {
-        print("async 3 : \(i)")
-    }
-}
+//new_dispatch.sync {
+//    for i in 0..<5 {
+//        print("new sync 1 : \(i)")
+//    }
+//}
+//
+//dispatch.sync {
+//    for i in 0..<5 {
+//        print("sync 1 : \(i)")
+//    }
+//}
+//
+//dispatch.async {
+//    for i in 0..<5 {
+//        print("async 2 : \(i)")
+//    }
+//}
+//
+//dispatch.async {
+//    for i in 0..<5 {
+//        print("async 3 : \(i)")
+//    }
+//}
 
 
 
