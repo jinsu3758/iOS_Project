462fdf3e48e7c76aabb9e85c0b77e9ef6095b350
jinsu3758 <jinsu3758@naver.com>
2021-01-30 00:02:32 +0900

    ClosureCapture : rx 테스트 unowned self의 위험

--- ClosureCapture/ClosureCapture/RxViewController.swift
+++ ClosureCapture/ClosureCapture/RxViewController.swift
@@ -15,7 +15,7 @@ class RxViewController: UIViewController {
     var disposeBag = DisposeBag()
     
     var value: Int = 0
-    let observable = Observable<Int>.of(1,2,3)
+    let observable = Observable<[Int]>.just([1,2,3])
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +23,38 @@ class RxViewController: UIViewController {
         // pop이 바로 안 됨
         // unowned self 가능함
         
-        observable
-            .subscribe(onNext: { item in
-                self.value = item
+        button.rx.tap
+            .subscribe(onNext: { [unowned self] in
+                print("onNext")
+                for _ in 0...10000 {
+                    self.value += 1
+                }
             }, onCompleted: {
-                print("onComplete")
-                self.value = 2
+                print("onCompleted")
+            }, onDisposed: { [unowned self] in
+                self.value += 1
+                print("onDisposed")
             })
-//            .disposed(by: disposeBag)
+            .disposed(by: disposeBag)
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            self.navigationController?.popViewController(animated: true)
+        })
+        
+//        observable
+//            .subscribe(onNext: { item in
+//                self.value = item.first!
+//            }, onCompleted: {
+//                print("onComplete")
+//                self.value = 2
+//            }, onDisposed: {
+//                print("onDisposed")
+//            })
+//            .disposed(by: disposeBag)
+//
 //        button.rx.tap
 //            .subscribe(onNext: { _ in
 //                self.value = 1
-////                self.doSomething()
-////                self.navigationController?.popViewController(animated: true)
 //            })
 //            .disposed(by: disposeBag)
         
@@ -66,7 +84,7 @@ class RxViewController: UIViewController {
             
 //        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
 //            self.navigationController?.popViewController(animated: true)
-//            
+//
 //        })
         
 //        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
