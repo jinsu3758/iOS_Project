1196706029e918c3aa907d3aa678722a7a736a8c
jinsu3758 <jinsu3758@naver.com>
2021-02-03 22:56:06 +0900

    scrollTest

---
+++ ScrollTest/ScrollTest/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ScrollTest
+//
+//  Created by 박진수 on 2020/12/04.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ ScrollTest/ScrollTest/uiview.swift
@@ -0,0 +1,38 @@
+//
+//  uiview.swift
+//  ScrollTest
+//
+//  Created by 박진수 on 2020/12/07.
+//
+
+import UIKit
+
+class uiview: UIView {
+
+//    override func addConstraint(_ constraint: NSLayoutConstraint) {
+//        <#code#>
+//    }
+//
+//    override var translatesAutoresizingMaskIntoConstraints: Bool
+//
+//    override func willMove(toWindow newWindow: UIWindow?) {
+//        <#code#>
+//    }
+//
+//    override func updateConstraints() {
+//        <#code#>
+//    }
+//
+//    override var intrinsicContentSize: CGSize
+//
+//    override func alignmentRect(forFrame frame: CGRect) -> CGRect {
+//        <#code#>
+//    }
+//    override func didAddSubview(_ subview: UIView) {
+//        <#code#>
+//    }
+//
+//    override func willRemoveSubview(_ subview: UIView) {
+//        <#code#>
+//    }
+}
